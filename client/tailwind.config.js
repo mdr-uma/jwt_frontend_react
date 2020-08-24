cedae50cc6309f55e0819762bcfcebdf22d0008a
Jennifer Pazos <jmp521@gmail.com>
2020-08-23 23:38:59 -0500

    added tailwind.css

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    plugins: [
+        require('@tailwindcss/ui'),
+    ]
+    // purge: [
+    //   'src/**/*.js',
+    //   'src/**/*.jsx',
+    //   'src/**/*.ts',
+    //   'src/**/*.tsx',
+    //   'public/**/*.html',
+    // ],
+    // theme: {
+    //   extend: {},
+    // },
+    // variants: {},
+    // plugins: [],
+  }
